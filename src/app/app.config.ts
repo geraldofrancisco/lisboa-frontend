9c69ed7268f09a415bc80a214705e4b9d29e063e
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2026-09-05 18:28:29 -0300

    adicionado primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,20 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    
+    // Configuração do PrimeNG
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
