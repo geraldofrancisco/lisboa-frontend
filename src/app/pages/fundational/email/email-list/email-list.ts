739920aae77ed50a38080df545c52b285ec0c8a0
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2026-09-07 20:35:40 -0300

    criado componente de email list

---
+++ src/app/pages/fundational/email/email-list/email-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-list',
+  imports: [],
+  templateUrl: './email-list.html',
+  styleUrl: './email-list.css',
+})
+export class EmailList {}
